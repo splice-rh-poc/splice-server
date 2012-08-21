608f5bfa90b9548a5a1619ef7352383ea86b9648
John Matthews <jmatthews@redhat.com>
2012-08-21 12:49:29 -0400

    test client script for https

---
+++ playpen/curl_scripts/https_client_checkin.sh
@@ -0,0 +1,5 @@
+export SERVER_ADDR=127.0.0.1
+export CONSUMER_IDENTITY="tbd_identifier"
+
+#curl -s -S -k -H "Content-Type: application/json" -X PUT --data '{"identity_cert":"TEXT_WOULD_GO_HERE", "products": ["PRODUCT1", "PRODUCT2"]}' http://${SERVER_ADDR}:${PORT}/api/v1/entitlement/${CONSUMER_IDENTITY}/ | python -mjson.tool
+curl -s -S -k --dump-header - -H "Content-Type: application/json" -X PUT --data '{"identity_cert":"TEXT_WOULD_GO_HERE", "consumer_identifier":"F0:DE:F1:DE:88:2B", "products": ["PRODUCT1", "PRODUCT2"]}' https://${SERVER_ADDR}/splice/api/v1/entitlement/${CONSUMER_IDENTITY}/ 
