8c6c818545ebe6eef23f4219f05a5ca049c9e7d8
John Matthews <jmatthews@redhat.com>
2012-08-21 12:56:26 -0400

    Update urls

--- playpen/curl_scripts/simple_client_checkin.sh
+++ playpen/curl_scripts/simple_client_checkin.sh
@@ -3,4 +3,4 @@ export PORT=8000
 export CONSUMER_IDENTITY="tbd_identifier"
 
 #curl -s -S -k -H "Content-Type: application/json" -X PUT --data '{"identity_cert":"TEXT_WOULD_GO_HERE", "products": ["PRODUCT1", "PRODUCT2"]}' http://${SERVER_ADDR}:${PORT}/api/v1/entitlement/${CONSUMER_IDENTITY}/ | python -mjson.tool
-curl -s -S -k --dump-header - -H "Content-Type: application/json" -X PUT --data '{"identity_cert":"TEXT_WOULD_GO_HERE", "consumer_identifier":"F0:DE:F1:DE:88:2B", "products": ["PRODUCT1", "PRODUCT2"]}' http://${SERVER_ADDR}:${PORT}/splice/api/v1/entitlement/${CONSUMER_IDENTITY}/ 
+curl -s -S -k --dump-header - -H "Content-Type: application/json" -X PUT --data '{"identity_cert":"TEXT_WOULD_GO_HERE", "consumer_identifier":"F0:DE:F1:DE:88:2B", "products": ["PRODUCT1", "PRODUCT2"]}' http://${SERVER_ADDR}:${PORT}/api/v1/entitlement/${CONSUMER_IDENTITY}/ 
