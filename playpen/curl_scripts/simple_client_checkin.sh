1fc6448c95683ab75ff093e960cfb006681ad02a
John Matthews <jmatthews@redhat.com>
2012-08-23 21:56:29 -0400

    Updating a test bash script to use curl and pass up a x509 cert, needs more work problem with 'Invalid control character' on server side when calling this, I think it's an issue with newlines in the x509 pem and it not playing nice when trying to decode the PUT data as JSON.

--- playpen/curl_scripts/simple_client_checkin.sh
+++ playpen/curl_scripts/simple_client_checkin.sh
@@ -1,6 +1,11 @@
 export SERVER_ADDR=127.0.0.1
 export PORT=8000
-export CONSUMER_IDENTITY="tbd_identifier"
-
-#curl -s -S -k -H "Content-Type: application/json" -X PUT --data '{"identity_cert":"TEXT_WOULD_GO_HERE", "products": ["PRODUCT1", "PRODUCT2"]}' http://${SERVER_ADDR}:${PORT}/api/v1/entitlement/${CONSUMER_IDENTITY}/ | python -mjson.tool
-curl -s -S -k --dump-header - -H "Content-Type: application/json" -X PUT --data '{"identity_cert":"TEXT_WOULD_GO_HERE", "consumer_identifier":"F0:DE:F1:DE:88:2B", "products": ["PRODUCT1", "PRODUCT2"]}' http://${SERVER_ADDR}:${PORT}/api/v1/entitlement/${CONSUMER_IDENTITY}/ 
+#
+# Need to fix server side error of 'Invalid control character ', I think this
+# is caused by embedded newline breaks in the cert.
+#
+export IDENTITY_CERT=`cat ../../src/splice/test_data/valid_cert/valid.cert`
+export CONSUMER_IDENTITY="dbcbc8e1-5b37-4a77-9db1-faf4ef29307d"
+export DATA="{\"identity_cert\":\"${IDENTITY_CERT}\", \"consumer_identifier\":\"F0:DE:F1:DE:88:2B\", \"products\": [\"PRODUCT1\", \"PRODUCT2\"]}"
+echo "DATA:  ${DATA}"
+curl -s -S -k --dump-header - -H "Content-Type: application/json" -X PUT --data "${DATA}" http://${SERVER_ADDR}:${PORT}/api/v1/entitlement/${CONSUMER_IDENTITY}/ 
