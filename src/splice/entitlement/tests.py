f9e982b9db5e03756d48952bd7ca6aa1af442eaa
John Matthews <jmatthews@redhat.com>
2012-08-28 16:41:27 -0400

    Unit test updates for lookup of RHIC to products, plus fix for unit tests to use test mongo database

--- src/splice/entitlement/tests.py
+++ src/splice/entitlement/tests.py
@@ -3,12 +3,14 @@ import os
 
 from tastypie.test import ResourceTestCase
 
-from mongoengine import connect
+from mongoengine.connection import connect, disconnect
 from django.conf import settings
 
 from splice.common import candlepin_client
 from splice.common.certs import CertUtils
+from splice.common.identity import create_new_consumer_identity
 from splice.entitlement.checkin import CheckIn, CertValidationException
+from splice.entitlement.models import ConsumerIdentity
 
 TEST_DATA_DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)), "..", "test_data")
 
@@ -20,8 +22,9 @@ class MongoTestCase(ResourceTestCase):
     """
     db_name = 'test_%s' % settings.MONGO_DATABASE_NAME
     def __init__(self, methodName='runtest'):
-        self.db = connect(self.db_name)
         super(MongoTestCase, self).__init__(methodName)
+        disconnect()
+        self.db = connect(self.db_name)
 
     def _post_teardown(self):
         super(MongoTestCase, self)._post_teardown()
@@ -56,6 +59,13 @@ class BaseEntitlementTestCase(MongoTestCase):
         # Expected data from an example session communicating with Candlepin
         self.expected_cert = "-----BEGIN CERTIFICATE-----\nMIID3zCCA0igAwIBAgICBNIwDQYJKoZIhvcNAQEFBQAwODEXMBUGA1UEAwwOaXAt\nMTAtNi05NC0xNDExCzAJBgNVBAYTAlVTMRAwDgYDVQQHDAdSYWxlaWdoMB4XDTEy\nMDgyNzE5NTc1NVoXDTEyMDgyNzIwNTc1NVowFzEVMBMGA1UEAxMMdGVzdHRlc3R0\nZXN0MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAsKDEY3dK0uc0ImSt\nSlQMfQzuR/iQgC3mHLapYPdqHRvX6w4PXx8uOKz1yMjuLj0/0GHtCod97VfAtL/h\nJ/FH9mMsR18y6qezcgbWKU4M4lffH3auQoCFUiJNlEi94d0qxH6iKyWqKRHveRa3\nerILvhMQG6np6XDt3gWirpRwZzi21qr5ZYh83brsuxzU6HfYB51jBYs0uJybxIfm\nDrpRa3YQiyPtE72v8IIcvqYDoXq9QzOVqAMOiw5BLpc0pKQVN6KeB8BYXEa5sfr/\nuQr+GhvlXWS/YWRZiqj6aW56vAKK+arvEbRKFkybJbnSNrfCt8ZKLrB7mZGbxySd\n1xvu7wIDAQABo4IBkzCCAY8wEQYJYIZIAYb4QgEBBAQDAgWgMAsGA1UdDwQEAwIE\nsDBoBgNVHSMEYTBfgBSl9RwXEephltcl32HNuZwR7ZAm16E8pDowODEXMBUGA1UE\nAwwOaXAtMTAtNi05NC0xNDExCzAJBgNVBAYTAlVTMRAwDgYDVQQHDAdSYWxlaWdo\nggkA0hXeS2SIlPMwHQYDVR0OBBYEFL6EQZpQ/Nm3DLW7ZM9UqMxv8VNpMBMGA1Ud\nJQQMMAoGCCsGAQUFBwMCMBwGCisGAQQBkggJBAEEDgwMUkhJQyBQcm9kdWN0MBYG\nCisGAQQBkggJBAIECAwGU1VCLUlEMBQGCisGAQQBkggJBAMEBgwEcmhpYzARBgor\nBgEEAZIICQQFBAMMATEwJAYKKwYBBAGSCAkEBgQWDBQyMDEyLTA4LTI3VDE5OjU3\nOjU1WjAkBgorBgEEAZIICQQHBBYMFDIwMTItMDgtMjdUMjA6NTc6NTVaMBEGCisG\nAQQBkggJBAwEAwwBMDARBgorBgEEAZIICQQOBAMMATAwDQYJKoZIhvcNAQEFBQAD\ngYEALdvvPEU3ozYse4NCSz+B3VrSrZ9Dv6vD+9yRqpXF/AWeYUgO4uyVc5fKzQDZ\nQTRXfqnvyJ773JOTUxJlYHt7uK2wGHbRefsRktGzLnwo3KZOhHWUXtMdt9bNzHw2\nt1rG3Jeubsw5v1s/Dhp5wHR0RvzrHnkxTVI/XFXbqnPvDJ0=\n-----END CERTIFICATE-----\n"
         self.expected_key = "-----BEGIN RSA PRIVATE KEY-----\nMIIEowIBAAKCAQEAsKDEY3dK0uc0ImStSlQMfQzuR/iQgC3mHLapYPdqHRvX6w4P\nXx8uOKz1yMjuLj0/0GHtCod97VfAtL/hJ/FH9mMsR18y6qezcgbWKU4M4lffH3au\nQoCFUiJNlEi94d0qxH6iKyWqKRHveRa3erILvhMQG6np6XDt3gWirpRwZzi21qr5\nZYh83brsuxzU6HfYB51jBYs0uJybxIfmDrpRa3YQiyPtE72v8IIcvqYDoXq9QzOV\nqAMOiw5BLpc0pKQVN6KeB8BYXEa5sfr/uQr+GhvlXWS/YWRZiqj6aW56vAKK+arv\nEbRKFkybJbnSNrfCt8ZKLrB7mZGbxySd1xvu7wIDAQABAoIBAGp7oaotguh/Bokt\nlONYpGbHYuf0zHjaMv5giOCyiECgRp2ozk+UZrM4Yrz3ldA+kLg6MUPFx39NBhpy\nC3WfGrlJtKoalIGvNJmr0XT5Xv8d5p/7Vrc08CtCDu80o3UFdwEbLv1AKcO00mB3\n94l6yWV/7Jgg6aoYsO7HCvqg9tri76Lc/ALblMfJ04oEvjHo3bo3OcQ5WQT/fhM+\nI1eIqulA1aPP79Wjta/KkeQ/WmKmvqz+eh9ukHdQQNdL1Hxa3sL0HgKbKknBQm2i\nKdPHLJGmc8yqv/0oJL8VZYYvtEOl/XOBZujJqI7WryA4YC9AJc0xjXP6DxrxrLOJ\n4MBChPECgYEA22RTQdjrmEWY/wRiqv0yPMJ0X81qUcpWFnwKQuZJf6811bi0hfej\n6o0h8H4x5PjVSWOTOirX6oI9mHUr5xERlt2YCuoTd/YDeXTSY68o1hKUwQm0RsV6\nN1mIoJ6D/qyuRp5BGKhhOJa5qB3Jbiwb7XEwg/181OXKi3VBG4lAkrMCgYEAzhm2\nuY8T4ep37VoYpQKJ91xm6qctfLxhiFzSWEjTFSJhYqp8cQn2HOovD692jUmczPDQ\nr6634ueSPF2+oRGFv3UlMtqKmEqCDD4dHl11+Xnry8kXjb7Pxq6oJDlRBjRMf2Mu\n4rHxDXpX9hv9S4nzwFoDr6JSYd8ygcqvtgWGoNUCgYEAwCYrQVWyaigyqs/1dtrR\ngCOzdyDbCI2VPpYfCp7VKq6TEP93pInTF5/KZO6x1mAVtfQvQ1e4ydyOBBRDgloR\ntLeZ7Z07tepS+rJVfhcwReX6QOO17/IPa6DQKBUNeCVXceQzEVyP4dco/dQw0nxx\nbVGgc0m1ZmVoMyJcBrj8RD8CgYBgtXAoYhrSR1M+7Kfjxe03RQSF1yxg+4RImEWb\nZ5Ckuh04TwdVg3cY2kp68bqPUZtiDx3dUf63WjIkYVix+6bmz/FEi4e9LjkXxY2k\nUfapuawLU7DZsk+Myyfa14pNfvzmSYQWm6igyme79CZG69SUzagtId3GTxVEEfeh\nUbZ6gQKBgERwlTIEZ6th8elckNl/Zso1g262xPy7GwX8IFlDCvY9ZAfoRz+aBWHK\nK4UbqPZmwXMsQJO7WVOdHoSl7pRPuS0196TEyYw3gu2rVAFernojX1EM0Q0eakYn\nPO75QiHsxdlIsfg1eNEXKqflCivXd3fDXuBtT/Q2MJBmE2ZkxTOB\n-----END RSA PRIVATE KEY-----\n"
+        self.checkin = CheckIn()
+        self.valid_products = ["40", "41"]
+        self.valid_identity_uuid = self.checkin.extract_id_from_identity_cert(self.valid_identity_cert_pem)
+        self.load_rhic_data()
+
+    def load_rhic_data(self):
+        create_new_consumer_identity(self.valid_identity_uuid, self.valid_products)
 
     def tearDown(self):
         super(BaseEntitlementTestCase, self).tearDown()
@@ -71,7 +81,8 @@ class EntitlementResourceTest(BaseEntitlementTestCase):
         # self.user = User.objects.create_user(self.username, 'admin@example.com', self.password)
         self.post_data = {
             'consumer_identifier': "52:54:00:15:E7:69",
-            'products': ["Product_1", "Product_2"],
+            'products': self.valid_products,
+            'system_facts': {"tbd":"values"}
             }
 
     def tearDown(self):
@@ -147,7 +158,6 @@ class CheckInTest(BaseEntitlementTestCase):
     """
     def setUp(self):
         super(CheckInTest, self).setUp()
-        self.checkin = CheckIn()
 
     def tearDown(self):
         super(CheckInTest, self).tearDown()
@@ -171,4 +181,22 @@ class CheckInTest(BaseEntitlementTestCase):
             self.checkin.extract_id_from_identity_cert(self.valid_identity_cert_pem),
             "dbcbc8e1-5b37-4a77-9db1-faf4ef29307d")
 
+    def test_check_access_allowed(self):
+        identity = ConsumerIdentity.objects(uuid=self.valid_identity_uuid).first()
+        allowed_products, unallowed_products = self.checkin.check_access(identity, self.valid_products)
+        for p in self.valid_products:
+            self.assertTrue(p in allowed_products)
+        self.assertEquals(len(unallowed_products), 0)
 
+    def test_check_access_unallowed(self):
+        identity = ConsumerIdentity.objects(uuid=self.valid_identity_uuid).first()
+        allowed_products, unallowed_products = self.checkin.check_access(identity, ["100", "101"])
+        self.assertTrue("100" in unallowed_products)
+        self.assertTrue("101" in unallowed_products)
+        self.assertEquals(len(allowed_products), 0)
+
+        allowed_products, unallowed_products = self.checkin.check_access(identity, [self.valid_products[0], "101"])
+        self.assertTrue(self.valid_products[0] in allowed_products)
+        self.assertTrue("101" in unallowed_products)
+        self.assertEquals(len(allowed_products), 1)
+        self.assertEquals(len(unallowed_products), 1)
