74289dd57b5c0fdb607ffa08a1ffeede94ebdf03
John Matthews <jmatthews@redhat.com>
2012-09-17 17:59:17 -0400

    Adding a celery task for syncing RHIC data

--- src/splice/entitlement/tasks.py
+++ src/splice/entitlement/tasks.py
@@ -4,6 +4,7 @@ sys.path.append("/etc/splice/celery")
 from celery import Celery
 
 from splice.common.constants import SPLICE_ENTITLEMENT_BASE_TASK_NAME
+from splice.common.identity import sync_from_rhic_serve_blocking
 import celeryconfig
 
 
@@ -21,3 +22,20 @@ def mul(x, y):
 @celery.task(name="%s.xsum" % SPLICE_ENTITLEMENT_BASE_TASK_NAME)
 def xsum(numbers):
     return sum(numbers)
+
+@celery.task(name="%s.sync_rhics")
+def sync_rhics(server_info=None):
+    """
+    Will synchronize RHIC to product mapping data from a RCS server.
+
+    @param server_info: optional, dict of information to connect to RCS server
+                        if no dict specified will retrieve info from local config file
+    @type  server_info: {}
+    @return: status of synchronization, with optional error message:
+             (True, "") - on success
+             (False, "error message here") - on failure
+    @rtype:  (bool,str)
+    """
+    return sync_from_rhic_serve_blocking()
+
+
