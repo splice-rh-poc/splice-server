9e3bec3309abe74662b338cebffc289bc9386bbe
John Matthews <jmatthews@redhat.com>
2012-10-05 14:00:59 -0400

    Touch up test script so we can vary gzip/limit and other params

--- playpen/rhic/stress_tests/get_rhics_from_localhost.py
+++ playpen/rhic/stress_tests/get_rhics_from_localhost.py
@@ -3,17 +3,36 @@ import logging
 import logging.config
 import os
 
+from optparse import OptionParser
+
 from splice.common import rhic_serve_client
 
 LOG_CONFIG_FILE=os.path.join(os.path.abspath(os.path.dirname(__file__)), "logging_config")
 logging.config.fileConfig(LOG_CONFIG_FILE)
 
-host = "localhost"
-port = "443"
-url = "/splice/api/v1/rhicrcs/"
 
-data, meta = rhic_serve_client.get_all_rhics(host=host, port=port, url=url, 
-        offset=0, limit=1000, debug=False, accept_gzip=True)
-print "Fetched %s rhics" % (len(data))
-print "Received meta data: %s" % (meta)
+def fetch(hostname, num_rhics, gzip_support=True, offset=0, port=443, url="/splice/api/v1/rhicrcs/"):
+    data, meta = rhic_serve_client.get_all_rhics(host=hostname, port=port, url=url, 
+        offset=offset, limit=num_rhics, debug=False, accept_gzip=gzip_support)
+    return data, meta
+
+if __name__ == "__main__":
+    parser = OptionParser(description="Test script to fetch RHICs from RCS")
+    parser.add_option("--num", action="store", default=10,
+            help="Number of RHICs to test with")
+    parser.add_option("--offset", action="store", default=0,
+            help="Offset of where to start rhic fetch from")
+    parser.add_option("--zip", action="store_true", default=False,
+            help="If set, will disable gzip support")
+    parser.add_option("--host", action="store", default="localhost",
+            help="hostname of RCS")
+    (opts, args) = parser.parse_args()
+    num_rhics = int(opts.num)
+    offset = int(opts.offset)
+    gzip_support = opts.zip
+    hostname = opts.host
+
+    data, meta = fetch(hostname, num_rhics, gzip_support, offset)
+    print "Fetched %s rhics" % (len(data))
+    print "Received meta data: %s" % (meta)
 
