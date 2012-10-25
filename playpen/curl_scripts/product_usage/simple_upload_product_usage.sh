bdffc062896858c82993f3bce29ef15caafc0ca8
John Matthews <jmatthews@redhat.com>
2012-10-25 14:19:34 -0400

    Moved location of HTTPS certs to use what python-certutils generates

--- playpen/curl_scripts/product_usage/simple_upload_product_usage.sh
+++ playpen/curl_scripts/product_usage/simple_upload_product_usage.sh
@@ -2,7 +2,7 @@
 
 export SERVER_ADDR=`hostname`
 export PORT=443
-export CA_CERT=/etc/pki/splice/Splice_CA.cert
+export CA_CERT=/etc/pki/splice/generated/Splice_CA.cert
 export DATA="[{
 \"consumer\":\"fb647f68-aa01-4171-b62b-35c2984a5328\", 
 \"instance_identifier\":\"F0:DE:F1:DE:88:2B\", 
