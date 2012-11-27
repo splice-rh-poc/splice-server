d2dc76bfc40e6de638626370228c21b8ff6ec6da
John Matthews <jmatthews@redhat.com>
2012-11-27 16:45:59 -0500

    Adding support for 'gzip' encoding to BaseConnection, also changed to return (status_code, response_body)

--- src/splice/common/connect.py
+++ src/splice/common/connect.py
@@ -12,15 +12,24 @@
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
 
 import base64
+import gzip
 import httplib
+import logging
 import simplejson as json
+import StringIO
 from M2Crypto import SSL, httpslib
 
 from splice.common.exceptions import RequestException
 
+# Hack to work around M2Crypto.SSL.Checker.WrongHost
+# seen when a remote server's SSL cert does not match their hostname
+SSL.Connection.clientPostConnectionCheck = None
+
+_LOG = logging.getLogger(__name__)
+
 class BaseConnection(object):
     def __init__(self, host, port, handler, username=None,
-                 password=None, https=True, cert_file=None, key_file=None, ca_cert=None):
+                 password=None, https=True, cert_file=None, key_file=None, ca_cert=None, accept_gzip=False):
         self.host = host
         self.port = int(port)
         self.handler = handler
@@ -32,6 +41,9 @@ class BaseConnection(object):
         self.cert_file = cert_file
         self.cert_key = key_file
         self.ca_cert  = ca_cert
+        self.accept_gzip = accept_gzip
+        if self.accept_gzip:
+            self.headers['Accept-Encoding'] = 'gzip'
 
     def set_basic_auth(self):
         raw = ':'.join((self.username, self.password))
@@ -62,14 +74,22 @@ class BaseConnection(object):
             # add the basic auth info to headers
             self.set_basic_auth()
         conn = self.__get_connection()
-        conn.request(request_type, self.handler + method, body=json.dumps(body), headers=self.headers)
+        url = self.handler + method
+        if body:
+            body = json.dumps(body)
+        _LOG.info("'%s' to '%s' \n\twith headers '%s'\n\t body '%s'" % \
+                  (request_type, url, self.headers, body))
+        conn.request(request_type, url, body=body, headers=self.headers)
         response = conn.getresponse()
-        if response.status not in [200, 202]:
-            raise RequestException(response.status, response.read())
-        data = response.read()
-        if not len(data):
-            return None
-        return json.loads(data)
+        response_body = response.read()
+        if response.getheader('content-encoding', '') == 'gzip':
+            data = StringIO.StringIO(response_body)
+            gzipper = gzip.GzipFile(fileobj=data)
+            response_body = gzipper.read()
+        _LOG.info("Received '%s' from '%s %s'" % (response.status, request_type, url))
+        if response.status in [200, 202]:
+            response_body = json.loads(response_body)
+        return response.status, response_body
 
     def GET(self, method):
         return self._request("GET", method)
